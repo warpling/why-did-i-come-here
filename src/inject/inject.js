40a569938c9940c761e2e73c69f625ee95c5fb34
Ryan McLeod <ryanmcleod@gmail.com>
2014-02-08 13:01:43 -0800

    Facebook.com now fades out! Still dealing with weird DS_Store stuff...

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -3,11 +3,12 @@ chrome.extension.sendMessage({}, function(response) {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
 
+        alert("test1");
 		// ----------------------------------------------------------
 		// This part of the script triggers when page is done loading
 		console.log("Hello. This message was sent from scripts/inject.js");
 		// ----------------------------------------------------------
-
+        // chrome.tabs.executeScript(null, {file: "overlay.js"});
 	}
 	}, 10);
 });
